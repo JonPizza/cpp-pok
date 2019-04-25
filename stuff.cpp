eb777a3c85a0399859f51ff994e881dc6e6129d0
JonPizza <45501389+JonPizza@users.noreply.github.com>
2019-04-25 12:50:38 -0400

    Create stuff.cpp

---
+++ stuff.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num {0};
+  cout << "Enter a number: ";
+  cin >> num;
+  cout << endl << "Thanks for the number!" << endl;
+  return 0;
+}
