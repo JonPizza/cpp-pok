6ecda04792c592f34bf3e4674019e4eb1be4cbeb
Jon <45501389+JonPizza@users.noreply.github.com>
2019-04-26 09:01:07 -0400

    Update stuff.cpp

--- stuff.cpp
+++ stuff.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 
 int main() {
-  int num {0};
-  cout << "Enter a number: ";
+  int num;
+  float answer;
+  cout << "Enter an int: ";
   cin >> num;
   cout << endl << "Thanks for the number!" << endl;
+  cout << "Now, you need to tell me wat exactly 1/2 of your number was: ";
+  cin >> answer;
+  if (answer == (num/2) {
+    cout << "Good Job!" << endl; 
+  } else {
+    cout << "WRONG!" << endl; 
+  }
   return 0;
 }
